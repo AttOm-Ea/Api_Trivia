6739cd7a95248a6e1dca945076ab747393660a81
Edwin Alberto <leonora.ea@gmail.com>
2020-10-22 23:07:01 -0500

    Uploat proyect

---
+++ Js/main.js
@@ -0,0 +1,142 @@
+
+    function getQuestion( ){
+        let questionQuantity = document.getElementById('inputSelect1').value;
+        let categoryQ = document.getElementById('inputSelect2').value;
+        let difficultyQ = document.getElementById('inputSelect3').value;
+        let typeQ = document.getElementById('inputSelect4').value;
+
+        fetch(`https://opentdb.com/api.php?amount=${questionQuantity}&category=${categoryQ}&difficulty=${difficultyQ}&type=${typeQ}`)
+        .then(response => response.json())
+        .then(data => pintCards(data.results))        
+    }   
+    
+    /* fetch(`https://opentdb.com/api.php?amount=10`)
+        .then(response => response.json())
+        .then(data => answers(data.results))
+    */ 
+    
+    function pintCards(questions){
+        let container = document.getElementsByClassName('content-card')[0];
+            clear();
+        let id = 0;
+        questions.forEach(question => {
+            
+            let card = cards(question, id);
+            container.innerHTML += card;
+            id++;            
+        });
+        let elemento = document.getElementsByClassName("contentBtn")[0];
+        if (questions.length == 0){
+            let cardEm = empty();
+            container.innerHTML += cardEm;
+            elemento.classList.remove('visible');
+        } else {
+            elemento.classList.add('visible');
+        }
+    }
+
+    function clear(){
+        let container = document.getElementsByClassName('content-card')[0];
+        container.innerHTML = " ";
+    }
+    function empty(){
+        let emAlert = `<div class="emergent"> Select another filter! </div>`
+        return emAlert;
+    }
+
+    function cards(q, id){
+        let card = `<div class="card text-white bg-info">
+                        <div class="card-header ch"> ${q.category} </div>
+                        <div class="card-body cb">
+                            <h5 class="cd-title"> ${q.question} </h5>
+                            <div class="input-group">
+                                <select class="custom-select" name="question-${id}" id="inputSelect-${id}" required> 
+                                    ${answers(q.correct_answer, q.incorrect_answers)}
+                                </select>
+                            </div>
+                        </div>
+                    </div>`
+        return card;
+    }
+    
+    function answers(corret, incorret){
+        let newArray = [ ];
+        newArray.push(corret);
+        incorret.forEach((incorretAns) => {
+            newArray.push(incorretAns);
+        })
+        const ansanswerDefault = ` <option> </option> `;
+        let answerR = '';
+        let answerQ = '';
+        const question = "question";
+        const quession = "quession";
+
+        if(newArray.length == 2){
+            let random = Math.floor(Math.random() * 2);
+                if(random == 0){
+                    answerR = `<option value=${question}> ${newArray[random]} </option>` 
+                } else{
+                    answerR = `<option value=${quession}> ${newArray[random]} </option>` 
+                }
+            if(random == 0){
+                answerQ = `<option value=${quession}> ${newArray[1]} </option> `;
+            } else{
+                answerQ = `<option value=${question}> ${newArray[0]} </option> `;
+            }
+        } else {
+            let random = Math.floor(Math.random() * 4);
+            if(random == 0){
+                answerR = `<option value=${question}> ${newArray[random]} </option>` 
+            } else{
+                answerR = `<option value=${quession}> ${newArray[random]} </option>` 
+            }
+            if(random == 0){
+                answerQ = `<option value=${quession}> ${newArray[3]} </option>
+                <option value=${quession}> ${newArray[2]} </option>
+                <option value=${quession}> ${newArray[1]} </option> `;
+            } else if(random == 1){
+                answerQ = `<option value=${question}> ${newArray[0]} </option>
+                <option value=${quession}> ${newArray[3]} </option>
+                <option value=${quession}> ${newArray[2]} </option> `;
+            } else if(random == 2){
+                answerQ = `<option value=${quession}> ${newArray[1]} </option>
+                <option value=${question}> ${newArray[0]} </option>
+                <option value=${quession}> ${newArray[3]} </option> `;
+            } else {
+                answerQ = `<option value=${quession}> ${newArray[2]} </option>
+                <option value=${quession}> ${newArray[1]} </option>
+                <option value=${question}> ${newArray[0]} </option> `;
+            }
+        }
+        let answer = ansanswerDefault + answerR + answerQ;
+        return answer;
+    }
+    function evaluation(){
+        let ModalBody = document.getElementsByClassName('modal-body')[0];
+        ModalBody.innerHTML = " ";
+        let questionQuantity = document.getElementById('inputSelect1').value;
+        let evaluation = 0;
+        let feedback = "";
+        for (let i = 0; i < questionQuantity; i++) {
+            let question = document.getElementById('inputSelect-' + i).value;
+            if(question == "question"){
+                feedback += `<p> The question ${i+1}  <samp class="green">is corret!</samp></p>`;
+                evaluation ++;
+            } else {
+                feedback += `<p> The question ${i+1} <samp class="red">is wrong!</samp></p>`
+            }
+            // location.reload();
+        }
+        ModalBody.innerHTML = feedback;
+
+        $('#exampleModal').modal()
+    }
+
+    
+            
+// Llamada windows
+    window.getQuestion = getQuestion;
+    window.answers = answers;
+    window.empty = empty;
+    window.clear = clear;
+    window.evaluation = evaluation;
---
+++ exercises.js
@@ -0,0 +1,252 @@
+// ------------------------- Tables ---------------------------------
+/*const tableBody = document.getElementById('Table-body');
+let table = false;
+// All Cars
+function printAll(){
+    tableBody.innerHTML = '';
+    cars.forEach(element => { 
+        const row = `<tr>
+                <td> ${element.brand} </td>
+                <td> ${element.model} </td>
+                <td> ${element.color} </td>
+                <td> ${element.age} </td>
+                <td> ${ '$' + element.price} </td>
+                <td class="button edit" onclick="editCarButton(${element.id})"> <span class="material-icons"> create </span> </td>
+                <td class="button remo" onclick="removeCar(${element.id})"> <span class="material-icons"> delete_forever </span> </td>
+            </tr> `;
+        tableBody.innerHTML += row;
+    });
+    table = "all"; 
+}
+// Nissan Model 
+function printNissan(){
+    tableBody.innerHTML = '';
+    let cleanNissan = cars.filter(cars => cars.brand == 'Nissan' || cars.brand == 'nissan');
+    cleanNissan.forEach(element => { 
+        const row = `<tr>
+                <td> ${element.brand} </td>
+                <td> ${element.model} </td>
+                <td> ${element.color} </td>
+                <td> ${element.age} </td>
+                <td> ${ '$' + element.price} </td>
+                <td class="button edit" onclick="editCarButton(${element.id})"> <span class="material-icons"> create </span> </td>
+                <td class="button remo" onclick="removeCar(${element.id})"> <span class="material-icons"> delete_forever </span> </td>
+            </tr> `;
+        tableBody.innerHTML += row;
+    });
+    table = "nissan"; 
+}
+// Ford Model 
+function printFord(){
+    tableBody.innerHTML = '';
+    let cleanFord = cars.filter(cars => cars.brand == 'Ford' || cars.brand == 'ford');
+    cleanFord.forEach(element => { 
+        const row = `<tr>
+                <td> ${element.brand} </td>
+                <td> ${element.model} </td>
+                <td> ${element.color} </td>
+                <td> ${element.age} </td>
+                <td> ${ '$' + element.price} </td>
+                <td class="button edit" onclick="editCarButton(${element.id})"> <span class="material-icons"> create </span> </td>
+                <td class="button remo" onclick="removeCar(${element.id})"> <span class="material-icons"> delete_forever </span> </td>
+            </tr> `;
+        tableBody.innerHTML += row;
+    });
+    table = "ford"; 
+} 
+// Chevrolet Model 
+function printChevrolet(){
+    tableBody.innerHTML = '';
+    let cleanChevrolet = cars.filter(cars => cars.brand == 'Chevrolet' || cars.brand == 'chevrolet' );
+    cleanChevrolet.forEach(element => { 
+        const row = `<tr>
+                <td> ${element.brand} </td>
+                <td> ${element.model} </td>
+                <td> ${element.color} </td>
+                <td> ${element.age} </td>
+                <td> ${ '$' + element.price} </td>
+                <td class="button edit" onclick="editCarButton(${element.id})"> <span class="material-icons"> create </span> </td>
+                <td class="button remo" onclick="removeCar(${element.id})"> <span class="material-icons"> delete_forever </span> </td>
+            </tr> `;
+        tableBody.innerHTML += row;
+    });
+    table = "chevrolet"; 
+}
+// More Age Model 
+function printAgeMore(){
+    tableBody.innerHTML = '';
+    let cleanAgeMore = cars.filter(cars => cars.age >= 2000);
+    cleanAgeMore.forEach(element => { 
+        const row = `<tr>
+                <td> ${element.brand} </td>
+                <td> ${element.model} </td>
+                <td> ${element.color} </td>
+                <td> ${element.age} </td>
+                <td> ${ '$' + element.price} </td>
+                <td class="button edit" onclick="editCarButton(${element.id})"> <span class="material-icons"> create </span> </td>
+                <td class="button remo" onclick="removeCar(${element.id})"> <span class="material-icons"> delete_forever </span> </td>
+            </tr> `;
+        tableBody.innerHTML += row;
+    });
+    table = "ageM"; 
+}
+// Less Age Model 
+function printAgeLess(){
+    tableBody.innerHTML = '';
+    let cleanAgeLess = cars.filter(cars => cars.age < 2000);
+    cleanAgeLess.forEach(element => { 
+        const row = `<tr>
+                <td> ${element.brand} </td>
+                <td> ${element.model} </td>
+                <td> ${element.color} </td>
+                <td> ${element.age} </td>
+                <td> ${ '$' + element.price} </td>
+                <td class="button edit" onclick="editCarButton(${element.id})"> <span class="material-icons"> create </span> </td>
+                <td class="button remo" onclick="removeCar(${element.id})"> <span class="material-icons"> delete_forever </span> </td>
+            </tr> `;
+        tableBody.innerHTML += row;
+    });
+    table = "ageL"; 
+}
+// More Price Model 
+function printPriceMore(){
+    tableBody.innerHTML = '';
+    let cleanPriceLess = cars.filter(cars => cars.price > 50000);
+    cleanPriceLess.forEach(element => { 
+        const row = `<tr>
+                <td> ${element.brand} </td>
+                <td> ${element.model} </td>
+                <td> ${element.color} </td>
+                <td> ${element.age} </td>
+                <td> ${ '$' + element.price} </td>
+                <td class="button edit" onclick="editCarButton(${element.id})"> <span class="material-icons"> create </span> </td>
+                <td class="button remo" onclick="removeCar(${element.id})"> <span class="material-icons"> delete_forever </span> </td>
+            </tr> `;
+        tableBody.innerHTML += row;
+    });
+    table = "priceM"; 
+}
+// Less Price Model 
+function printPriceLess(){
+    tableBody.innerHTML = '';
+    let cleanPriceLess = cars.filter(cars => cars.price <= 50000);
+    cleanPriceLess.forEach(element => { 
+        const row = `<tr>
+                <td> ${element.brand} </td>
+                <td> ${element.model} </td>
+                <td> ${element.color} </td>
+                <td> ${element.age} </td>
+                <td> ${ '$' + element.price} </td>
+                <td class="button edit" onclick="editCarButton(${element.id})"> <span class="material-icons"> create </span> </td>
+                <td class="button remo" onclick="removeCar(${element.id})"> <span class="material-icons"> delete_forever </span> </td>
+            </tr> `;
+        tableBody.innerHTML += row;
+    });
+    table = "priceL";
+}
+// ------------------------------ Form ----------------------------------
+let editingCar = false;
+function submitCar(){
+    if(editingCar){
+        editCar();
+    }else{
+        addCar();
+    }
+}
+function addCar(){
+    // Recibo de values
+    const Brand = document.getElementById('Brand').value;
+    const Model = document.getElementById('Model').value;
+    const Color = document.getElementById('Color').value;
+    const Age = document.getElementById('Age').value;
+    const Price = document.getElementById('Price').value;
+    const Id = cars.length + 1;
+    // Push new car
+    const newCar = {id: Id, brand: Brand, age: Age, color: Color, model: Model, price: Price};
+    cars.push(newCar);
+    console.log(cars);
+    // Print all car
+    printAll();   
+}
+function removeCar(id){
+    // Position on Array
+    const position =  cars.findIndex((car) => car.id === id);
+    // Delete Car 
+    cars.splice(position, 1);
+    // Print all car
+    switch (table){
+        case "all":
+            printAll();
+        break;
+        case "nissan":
+            printNissan();
+        break;
+        case "ford":
+            printFord();
+        break;
+        case "chevrolet":
+            printChevrolet();
+        break;
+        case "ageM":
+            printPriceMore();
+        break;
+        case "ageL":
+            printAgeLess();
+        break;
+        case "priceM":
+            printPriceMore();
+        break;
+        case "priceL":
+            printPriceLess();
+        break;
+    }
+}
+function editCarButton(id){
+    // Position on Array
+    const car =  cars.find((car) => car.id === id);
+    // Value on array
+    document.getElementById('Brand').value = car.brand;
+    document.getElementById('Model').value = car.model;
+    document.getElementById('Color').value = car.color;
+    document.getElementById('Age').value = car.age;
+    document.getElementById('Price').value = car.price;
+    // Reture Selet Car 
+    editingCar = car;
+}
+function editCar(){
+    // Recibo de values
+    const Brand = document.getElementById('Brand').value;
+    const Model = document.getElementById('Model').value;
+    const Color = document.getElementById('Color').value;
+    const Age = document.getElementById('Age').value;
+    const Price = document.getElementById('Price').value;
+    // asginación 
+    editingCar.brand = Brand;
+    editingCar.model = Model;
+    editingCar.color = Color;
+    editingCar.age = Age;
+    editingCar.price = Price;
+    printAll();
+    editingCar = false;
+    document.getElementById('Brand').value = ' ';
+    document.getElementById('Model').value = ' ';
+    document.getElementById('Color').value = ' ';
+    document.getElementById('Age').value = ' ';
+    document.getElementById('Price').value = ' ';
+}
+printAll();
+// ------------------------- PrintWindow ---------------------------------
+window.printAll = printAll;
+window.printNissan = printNissan;
+window.printFord = printFord;
+window.printChevrolet = printChevrolet;
+window.printAgeMore = printAgeMore;
+window.printAgeLess = printAgeLess;
+window.printPriceMore = printPriceMore;
+window.printPriceLess = printPriceLess;
+window.addCar = addCar;
+window.submitCar = submitCar;
+window.removeCar = removeCar;
+window.editCarButton = editCarButton;
+window.editCar = editCar;
+*/
